fa3f0e2a00c9ad09f5a186be13cf08b212f8caba
Matt Lobo <ml1994@princeton.edu>
2023-11-08 20:42:55 -0500

    updated functions

--- mjcl_stab.jl
+++ mjcl_stab.jl
@@ -1,17 +1,17 @@
 module LinStab
 
-export
-    lin_stab
+using FFTW, LinearAlgebra, Statistics, Peaks
 
 ## define stability functions
 
-function lin_stab(U::Vector{Float64},V::Vector{Float64},beta,eta,Nx::Int64,Ny::Int64,rho::Vector{Float64},f0::Float64,Lx::Float64,Ly::Float64)
+function lin_stab(U::Vector{Float64},V::Vector{Float64},H,beta,eta,Nx::Int64,Ny::Int64,rho::Vector{Float64},f0::Float64,g,Lx::Float64,Ly::Float64)
     # U: (Nx x Nz) vector of zonal mean background velocity
     # V: (Ny x Nz) vector of meridional mean background velocity
     # beta: 
     # 
 
     Nz = length(rho)
+
     # define wavenumbers
     k_x = reshape(fftfreq(Nx, 2π/Lx*Nx),(1,Nx))
     k_y = reshape(fftfreq(Ny, 2π/Ly*Ny),(1,Ny))
@@ -21,7 +21,7 @@ function lin_stab(U::Vector{Float64},V::Vector{Float64},beta,eta,Nx::Int64,Ny::I
     # k2 = k_x.^2 + k_y.^2            # this is for an isotropic wavenumber grid only (i.e. Nx=Ny)
 
     # define stretching matrix
-    S = calc_stretching_mat(Nz,rho,f0,H,rho[1])
+    S = calc_stretching_mat(Nz,rho,f0,H,rho[1],g)
 
     # change dimensions of U and V to match domain size
     U2 = zeros(1,Nz); U2[:] = U; U2 = repeat(U2,outer=(Ny,1))
@@ -30,63 +30,69 @@ function lin_stab(U::Vector{Float64},V::Vector{Float64},beta,eta,Nx::Int64,Ny::I
     V2 = zeros(1,Nz); V2[:] = V; V2 = repeat(V2,outer=(Nx,1))
     V = zeros(1,Nx,Nz); V[1,:,:] = V2
 
-    # define background QG PV gradients
+    # define background QG PV gradient
     Qy = calc_PV_grad_y(U,beta,eta,Ny,Nz,k_y,S)
     Qx = calc_PV_grad_x(V,eta,Nx,Nz,k_x,S)
 
     # perform linear stability analysis
-    evecs_all,evals_all = calc_lin_stab(Qy,Qx,U,V,S,k_x,k_y,Nz)
+    evecs_all,evals_all = calc_lin_stab(Qy,Qx,U,V,S,k_x,k_y,Nx,Ny,Nz)
 
     # keep largest growth rates per wavenumber
     evecs,evals,max_evec,max_eval = find_growth(evecs_all,evals_all,Nx,Ny,Nz)
 
     # def rad
-    r_d = sqrt(gp(rho[1:2],rho[1])*H[1])/f0
+    evals_S = eigvals(-S); evecs_S = eigvecs(-S)
+    sort_ind = sortperm(abs.(evals_S))
 
-    return fftshift(evecs),fftshift(evals),max_evec,max_eval,fftshift(k_x),fftshift(k_y),mean(Qx[1,:,:],dims=1),mean(Qy[1,:,:],dims=1)
+    # T2 = eltype(eigvals)
+    r_d = Complex.(zeros(Nz,1))
+    r_d[1] = sqrt(g*sum(H))/f0
+    r_d[2:end] = @. sqrt(Complex(evals_S[sort_ind[2:end]]))^-1
+
+    return fftshift(evecs),fftshift(evals),max_evec,max_eval,fftshift(k_x),fftshift(k_y),mean(Qx[1,:,:],dims=1),mean(Qy[1,:,:],dims=1),r_d
 end
 
-function lin_stab(U::Vector{Float64},V::Vector{Float64},beta,eta,Nx::Int64,Ny::Int64,rho::Vector{Float64},f0::Float64,Lx::Float64,Ly::Float64,Qy)
-    # Takes Qy as arg
-    # U: (Nx x Nz) vector of zonal mean background velocity
-    # V: (Ny x Nz) vector of meridional mean background velocity
-    # beta: 
-    # 
+# function lin_stab(U::Vector{Float64},V::Vector{Float64},H,beta,eta,Nx::Int64,Ny::Int64,rho::Vector{Float64},f0::Float64,g,Lx::Float64,Ly::Float64,Qy)
+#     # Takes Qy as arg; to mesh with GFJL output
+#     # U: (Nx x Nz) vector of zonal mean background velocity
+#     # V: (Ny x Nz) vector of meridional mean background velocity
+#     # beta: 
+#     # 
 
-    Nz = length(rho)
-    # define wavenumbers
-    k_x = reshape(fftfreq(Nx, 2π/Lx*Nx),(1,Nx))
-    k_y = reshape(fftfreq(Ny, 2π/Ly*Ny),(1,Ny))
+#     Nz = length(rho)
+#     # define wavenumbers
+#     k_x = reshape(fftfreq(Nx, 2π/Lx*Nx),(1,Nx))
+#     k_y = reshape(fftfreq(Ny, 2π/Ly*Ny),(1,Ny))
 
-    # k_x,k_y = wavenumber_grid(Nx,Ny,Lx,Ly)
+#     # k_x,k_y = wavenumber_grid(Nx,Ny,Lx,Ly)
 
-    # k2 = k_x.^2 + k_y.^2            # this is for an isotropic wavenumber grid only (i.e. Nx=Ny)
+#     # k2 = k_x.^2 + k_y.^2            # this is for an isotropic wavenumber grid only (i.e. Nx=Ny)
 
-    # define stretching matrix
-    S = calc_stretching_mat(Nz,rho,f0,H,rho[1])
+#     # define stretching matrix
+#     S = calc_stretching_mat(Nz,rho,f0,H,rho[1],g)
 
-    # change dimensions of U and V to match domain size
-    U2 = zeros(1,Nz); U2[:] = U; U2 = repeat(U2,outer=(Ny,1))
-    U = zeros(1,Ny,Nz); U[1,:,:] = U2
+#     # change dimensions of U and V to match domain size
+#     U2 = zeros(1,Nz); U2[:] = U; U2 = repeat(U2,outer=(Ny,1))
+#     U = zeros(1,Ny,Nz); U[1,:,:] = U2
 
-    V2 = zeros(1,Nz); V2[:] = V; V2 = repeat(V2,outer=(Nx,1))
-    V = zeros(1,Nx,Nz); V[1,:,:] = V2
+#     V2 = zeros(1,Nz); V2[:] = V; V2 = repeat(V2,outer=(Nx,1))
+#     V = zeros(1,Nx,Nz); V[1,:,:] = V2
 
-    # define background QG PV gradients (TEMPORARY)
-    Qy = reshape(Qy[1,:,:],(1,Ny,Nz))
-    Qx = zeros(size(Qy))
+#     # define background QG PV gradients (TEMPORARY)
+#     Qy = reshape(Qy[1,:,:],(1,Ny,Nz))
+#     Qx = zeros(size(Qy))
 
-    # perform linear stability analysis
-    evecs_all,evals_all = calc_lin_stab(Qy,Qx,U,V,S,k_x,k_y,Nz)
+#     # perform linear stability analysis
+#     evecs_all,evals_all = calc_lin_stab(Qy,Qx,U,V,S,k_x,k_y,Nx,Ny,Nz)
 
-    # keep largest growth rates per wavenumber
-    evecs,evals,max_evec,max_eval = find_growth(evecs_all,evals_all,Nx,Ny,Nz)
+#     # keep largest growth rates per wavenumber
+#     evecs,evals,max_evec,max_eval = find_growth(evecs_all,evals_all,Nx,Ny,Nz)
 
-    # def rad
-    r_d = sqrt(gp(rho[1:2],rho[1])*H[1])/f0
+#     # def rad
+#     r_d = sqrt(gp(rho[1:2],rho[1],g)*H[1])/f0
 
-    return fftshift(evecs),fftshift(evals),max_evec,max_eval,fftshift(k_x),fftshift(k_y),mean(Qx[1,:,:],dims=1),mean(Qy[1,:,:],dims=1)
-end
+#     return fftshift(evecs),fftshift(evals),max_evec,max_eval,fftshift(k_x),fftshift(k_y),mean(Qx[1,:,:],dims=1),mean(Qy[1,:,:],dims=1)
+# end
 
 function wavenumber_grid(Nx,Ny,Lx,Ly)
     #
@@ -142,53 +148,35 @@ function calc_PV_grad_x(V,eta,Nx::Int64,Nz::Int64,k_x,S)
     return Q_x
 end
 
-function gp(rho,rho0)
-    g = 9.81
+function gp(rho,rho0,g)
+    # g_prime = g*(rho[2]-rho[1])/rho0
     g_prime = g*(rho[2]-rho[1])/rho0
-
     return g_prime
 end
 
-function calc_stretching_mat(Nz,rho,f0,H,rho0)
+function calc_stretching_mat(Nz,rho,f0,H,rho0,g)
     #
     S = zeros((Nz,Nz,))
 
     alpha = 0
+    eta_b_x = 0
 
-    S[1,1] = -f0^2/H[1]/gp(rho[1:2],rho0) + alpha
-    S[1,2] = f0^2/H[1]/gp(rho[1:2],rho0)
+    S[1,1] = -f0^2/H[1]/gp(rho[1:2],rho0,g) + alpha
+    S[1,2] = f0^2/H[1]/gp(rho[1:2],rho0,g)
+    
     for i = 2:Nz-1
-        S[i,i-1] = f0^2/H[i]/gp(rho[i-1:i],rho0)
-        S[i,i]   = -(f0^2/H[i]/gp(rho[i:i+1],rho0) + f0^2/H[i]/gp(rho[i-1:i],rho0))
-        S[i,i+1] = f0^2/H[i]/gp(rho[i:i+1],rho0)
+        S[i,i-1] = f0^2/H[i]/gp(rho[i-1:i],rho0,g)
+        S[i,i]   = -(f0^2/H[i]/gp(rho[i-1:i],rho0,g) + f0^2/H[i]/gp(rho[i:i+1],rho0,g))
+        S[i,i+1] = f0^2/H[i]/gp(rho[i:i+1],rho0,g)
     end
-    S[Nz,Nz-1] = f0^2/H[Nz]/gp(rho[Nz-1:Nz],rho0)
-    S[Nz,Nz]   = -f0^2/H[Nz]/gp(rho[Nz-1:Nz],rho0)
+
+    S[Nz,Nz-1] = f0^2/H[Nz]/gp(rho[Nz-1:Nz],rho0,g)
+    S[Nz,Nz]   = -f0^2/H[Nz]/gp(rho[Nz-1:Nz],rho0,g) + eta_b_x
 
     return S
 end
 
-function calc_lin_stab(Qy,Qx,U,V,S,k_x,k_y,Nz)
-    #
-    # A = (k_x .* U .+ k_y .* V)
-
-    # B   = zeros((1,length(k2),Nz))
-    # ell = zeros((Nz,Nz,length(k_x),length(k_y)))
-    # for i=eachindex(k_x)
-    #     for j=eachindex(k_y)
-    #         ell[:,:,i,j] = S .- (k_x[i]^2 + k_y[j]^2) * I
-    #         B[1,i,j,:] = ell[:,:,i,j] * A[1,i,:]
-    #     end
-    # end
-
-    # C = k_x .* Qy .- k_y .* Qx
-
-    # M = inv(ell) * (A .+ C)
-
-    # evecs,evals = eig(M)
-
-
-    #################
+function calc_lin_stab(Qy,Qx,U,V,S,k_x,k_y,Nx,Ny,Nz)
     evecs = zeros(Nx,Ny,Nz,Nz) .+ 0im
     evals = zeros(Nx,Ny,Nz) .+ 0im
     k2    = zeros(Nx,Ny)
@@ -199,11 +187,6 @@ function calc_lin_stab(Qy,Qx,U,V,S,k_x,k_y,Nz)
                 # do nothing
             else
                 A = make_diag(k_x[i] .* U[:,j,:] .+ k_y[j] .* V[:,i,:]) 
-                # ell_i = inv(S - (k_x[i]^2 + k_y[j]^2)*I)
-                # B = ell_i .* make_diag(k_x[i] * Qy[:,j,:] - k_y[j] * Qx[:,i,:]) .+ 0im
-
-                # evecs[i,j,:,:] = eigvecs(A .+ B)
-                # evals[i,j,:] = eigvals(A .+ B)
 
                 ell = (S - (k_x[i]^2 + k_y[j]^2) * I)
                 A2 = ell * A
@@ -252,6 +235,28 @@ function find_growth(evecs_all,evals_all,Nx,Ny,Nz)
     max_evec = abs.(evecs[indMax,:])
 
     return evecs,evals,max_evec,max_eval
+end
+
+"""
+    calc_growth(t, E_in)
+Estimate the growth rate of instabilities from a least-squares fit to energy
+histories, where E_in = [KE1 KE2 KE3 PE32 PE52]
+MODIFIED FROM @APALOCZY
+"""
+function calc_growth(t, E_in)
+  E_tot = sum(E_in,dims=2)
+  min_ind,trash = Peaks.findminima(diff(E_tot[:],dims=1))
+  f = min_ind[end] + 1 # get first time index from the last renormalization cycle
+  t, KE1_new = t[f:end], E_in[f:end,1]           # construct time series from last section of growth in upper layer
+  n = size(t)[1]
+  d = Matrix(reshape(log.(KE1_new), (1, n)))
+  gm = Matrix(reshape(t, (1, n)))
+  Gm = Matrix([ones(n, 1) gm'])
+  GmT = Gm'
+  mv = inv(GmT*Gm)*(GmT*d')
+  sigma = mv[2]/2
+
+  return sigma
 end
 
 end # (module)
